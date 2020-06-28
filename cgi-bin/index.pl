67f7a5bfa3207a630fc1db700f4845c38651de49
sam <sam@localhost.localdomain>
2020-06-27 20:46:53 -0500

    updates to install instructions

--- cgi-bin/index.pl
+++ cgi-bin/index.pl
@@ -15,7 +15,7 @@ print "
 	</head>
 	<body>
 		<p>You can follow instructions here to make this page run
-		on a centos 7/8 [probably most redhat distro] with
+		on a centos 7/8 install [probably most redhat distros] with
 		Apache [httpd]</p>
 
 		<a href=\"https://www.server-world.info/en/note?os=CentOS_7&p=httpd&f=2\">Link to Server World [.info] page]</a>
@@ -25,12 +25,20 @@ print "
 		get Perl CGI + Apache installed on CentOS. Additionally, you'll
 		need to  point the default [localhost or domain] host in Apache
 		at the github repository files.</p>
+		<p>You may also need to check with <pre>sestatus</pre> to see
+		if you need to modify SELinux permissions for httpd with this fix:
+		<pre>semanage fcontext -a -t httpd_sys_script_exec_t \"/var/www/html/cgi-test(/.*)?\"</pre>
+		and update the rules with this
+		<pre>restorecon -R -v /var/www/html/cgi-test</pre>
+		replacing <pre>cgi-test</pre> with the location of the downloaded
+		github repo <pre>PerlTTT/cgi-bin</pre>
+		</p>
 
 		<p>this link is the reason for this page, I'll be loading a
 		React.JS interface to play tic-tac-toe with (Ideally using a
 		mini-max algorithm for the autonomous player, using this api
 		with JSON inputs/outputs </p>
-		<a href=\"/cgi-bin/tic_tac_toe_api.pl\">Tic-Tac-Toe API</a>
+		<a href=\"/cgi-bin/tic-tac-toe-api.pl\">Tic-Tac-Toe API</a>
 
 	</body>
 </html>";
---
+++ cgi-bin/tic-tac-toe-api.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use diagnostics;
+
+
+#headers end with \n\n
+print "Content-type: text/json";
+print "\n\n";
