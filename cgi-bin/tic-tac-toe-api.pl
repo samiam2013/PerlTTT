fb4389296c33816d89ecaea5d5ca226f72ae0d92
sam <sam@localhost.localdomain>
2020-06-30 00:32:38 -0500

    there's a logic bug somewhere

--- cgi-bin/tic-tac-toe-api.pl
+++ cgi-bin/tic-tac-toe-api.pl
@@ -56,6 +56,7 @@ sub check_win {
       }
     }
   }
+  return 0
 }
 
 sub move_left {
@@ -69,21 +70,104 @@ sub move_left {
   return 0;
 }
 
+sub find_best_move {
+  my $board = shift(@_);
+  if ($board->[4] eq "_"){
+    return 4;
+  } else {
+    my $best_val = -20;
+    my $best_move = -1;
+    for (0..8) {
+      #print "checking move ".$_."\n";
+      if ($board->[$_] eq "_"){
+        #print "trying minimax on move\n";
+
+        $board->[$_] = "X";
+        my $move_val = minimax($board,0,0);
+        $board->[$_] = "_";
+        if ($move_val > $best_val) {
+          $best_val = $move_val;
+          $best_move = $_;
+        }
+
+      }
+    }
+    print "best move: ".$best_move."\n";
+    return $best_move;
+  }
+}
+
+
+sub minimax{
+  my $board = shift(@_);
+  my $depth = shift(@_);
+  my $is_max = shift(@_);
+  #print "depth: ".$depth."\n";
+  #print "is_max: ".$is_max."\n";
+  my $win_score = check_win($board);
+  if ($win_score != 0){
+    #print "winner caught!\n";
+    return $win_score;
+  }
+  if (move_left($board) == 0){
+    #print "draw caught!\n";
+    return 0;
+  }
+
+  #logic for recursive scoring
+  if ($is_max == 1) { # player O, computer
+    my $best_val = -20;
+    my $max_val;
+    for (0..8){
+      if ($board->[$_] eq "_"){
+        $board->[$_] = "O";
+        $max_val = minimax($board,$depth+1,0);
+        if ($max_val > $best_val){
+          $best_val = $max_val;
+        }
+        $board->[$_] = "_";
+      }
+    }
+    return $best_val;
+  } else { #minimizer, player x
+    my $best_val = 20;
+    my $min_val;
+    for (0..8){
+      if ($board->[$_] eq "_"){
+        $board->[$_] = "O";
+        my $min_val = minimax($board,$depth+1,1);
+        if ($min_val < $best_val){
+          $best_val = $min_val;
+        }
+        $board->[$_] = "_";
+      }
+    }
+    return $best_val;
+  }
+
+
+}
+
+
 #print "move_left():".move_left($input_board)."\n";
 my $score = check_win($input_board);
+my @response = ("unknown",-1);
+#               (move type, move space)
 if ($score == -10){ #no winner yet
   #print "winner: X";
-  # send game over signal?
-} elsif ($score == 10){
-  #print "winner: O";
-  # send game over signal?
+  $response[0] = "winner X";
 } elsif ($score == 0){
-  #print "no winner yet";
   if (move_left($input_board)) {
     # find next move
     print "computing next move! ......\n";
+    my $next_move = find_best_move($input_board);
+    $response[0] = "continue";
+    $response[1] = $next_move;
   } else {
     #print "draw"
-    # send game over signal?
+    $response[0] = "draw";
   }
 }
+#print Dumper \@response;
+my $response_json = $json->encode(\@response);
+print $response_json;
